292b8272e008bb9a135821a54f03ec9b8424226c
Millington-Holi7 <holinas007@gmail.com>
2024-04-15 19:15:06 -0400

    Updated the file with parameterized queries.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -45,18 +45,23 @@ LIMIT 5;
   });*/
   
 
-  
+
   // Using command line argument to create a query to get info from the tables
-pool
-  .query(
-    `
-    SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-    FROM students
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-    `
-  )
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+  const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 
   .then((res) => {
     res.rows.forEach((user) => {
